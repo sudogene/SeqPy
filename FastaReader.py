e73e152a5901345bca620600f45e1295bd07ce1a
sudogene <andywubby@gmail.com>
2020-07-31 18:28:00 +0800

    created FastaRecord -> dict + str combination

--- FastaReader.py
+++ FastaReader.py
@@ -1,10 +1,19 @@
 from Seq import *
+import re
 
 class FastaReader:
+    """
+    FastaReader parses fasta-formatted files to store all 
+    fasta records as FastaRecords. Can be iterated over in a loop
+    to retrieve all records.
+    """
+    _extensions = {'txt', 'fasta', 'fa'}
     _classes = {'Seq': Seq, 'DNA': DNA, 'RNA': RNA, 'Protein': Protein}
 
+
     def __init__(self, filename, seq_type='Seq'):
-        if not filename.endswith('.fasta') or seq_type not in FastaReader._classes.keys():
+        if filename.split('.')[-1] not in self._extensions or \
+        seq_type not in self._classes.keys():
             raise ValueError("Invalid input")
 
         self._sequences = []
@@ -16,7 +25,9 @@ class FastaReader:
                 line = line.rstrip('\n')
                 if line.startswith('>'):
                     if seq:
-                        self._sequences.append((header, FastaReader._classes[seq_type](seq)))
+                        fo = FastaRecord(header, self._classes[seq_type](seq))
+
+                        self._sequences.append(fo)
                         seq = ''
                         header = line
                     else:
@@ -24,18 +35,74 @@ class FastaReader:
                 else:
                     seq += line
                 line = f.readline()
-            self._sequences.append((header, FastaReader._classes[seq_type](seq)))
+            fo = FastaRecord(header, self._classes[seq_type](seq))
+            self._sequences.append(fo)
+
 
     def __repr__(self):
-        return repr(self._sequences)
+        return f"FastaReader[{len(self._sequences)}]"
+
 
     def __len__(self):
         return len(self._sequences)
     
+
     def __getitem__(self, other):
         return self._sequences[other]
+    
 
-
-class FastaObject:
-    def __init__(self, string):
+class FastaRecord:
+    """
+    FastaRecord contains a dictionary of the description of the 
+    fasta entity, usually found within the header, and the sequence.
+    
+    The record can be queried like a dictionary, if needed, by following
+    the standard dictionary functions such as keys(), values(), items(),
+    and the dunder __getitem__ method. Sequence can be retrieved via seq.
+    """
+    def __init__(self, header, seq):
+        self.desc = {'id': header.split()[0]}
+        descs = list(re.findall(r"[^[]*\[([^]]*)\]", header))
+        for d in descs:
+            k, v = d.split('=')
+            self.desc[k] = v
+        self.seq = seq
         
+
+    def __repr__(self):
+        return f"Fasta({self.desc['id']})"
+    
+
+    def __len__(self):
+        return len(self.seq)
+    
+
+    def describe(self):
+        d = []
+        for k, v in self.desc.items():
+            d.append(f"{k}={v}")
+        print("\n".join(d))
+
+
+    def __getitem__(self, k):
+        try:
+            return self.desc[k]
+        except KeyError:
+            raise KeyError("Call keys() for valid keys")
+    
+
+    def keys(self):
+        return self.desc.keys()
+    
+
+    def values(self):
+        return self.desc.values()
+    
+
+    def items(self):
+        return self.desc.items()
+
+
+if __name__ == '__main__':
+    fr = FastaReader('sequence.fasta')
+    
